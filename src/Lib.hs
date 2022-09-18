c35694e24ccc1bd02538cf55c206078bdea65fa8
ben lim <lim6112j@gmail.com>
2022-09-18 06:51:19 +0000

    applicative

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@
 
 module Lib
   ( someFunc,
+    someFunc2,
   )
 where
 
@@ -13,9 +14,21 @@ doubleStrNumber str =
     Just x -> Just (2 * x)
     Nothing -> Nothing
 
+doubleStrNumber2 :: (Num b, Read b) => String -> Maybe b
+doubleStrNumber2 str = (* 2) `fmap` readMaybe str
+
+plusStrNumbers :: (Num b, Read b) => String -> String -> Maybe b
+plusStrNumbers s1 s2 = (+) <$> readMaybe s1 <*> readMaybe s2
+
 printMaybe :: Show a => Maybe a -> String
 printMaybe (Just a) = show a
 printMaybe Nothing = "Nothing"
 
 someFunc :: IO ()
-someFunc = putStrLn $ printMaybe $ doubleStrNumber "3"
+-- someFunc = putStrLn $ printMaybe $ doubleStrNumber "3"
+
+-- somefunc = print $ doubleStrNumber "3"
+someFunc = print $ doubleStrNumber2 "3"
+
+someFunc2 :: IO ()
+someFunc2 = print $ plusStrNumbers "3" "4"
