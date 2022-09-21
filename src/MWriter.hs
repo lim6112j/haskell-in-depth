6736df637385ab259332354e98085ecf4cab9b8e
lim6112j <lim6112j@gmail.com>
2022-09-22 01:34:33 +0900

    writer monad finished

--- src/MWriter.hs
+++ src/MWriter.hs
@@ -3,16 +3,29 @@
 {-# HLINT ignore "Use camelCase" #-}
 
 -- | gcdM
-module MWriter (gcdM, gcd_logSteps, gcd_run) where
+module MWriter where
 
 import Control.Monad.Writer
 
+-- using plain function : step e.g. print (a,b)
 gcdM :: (Integral a, Monad m) => (a -> a -> m ()) -> a -> a -> m a
 gcdM step a 0 = step a 0 >> pure a
 gcdM step a b = step a b >> gcdM step b (a `mod` b)
 
+-- log arraay of pair
 gcd_logSteps :: Integral a => a -> a -> Writer [(a, a)] a
 gcd_logSteps = gcdM (\a b -> tell [(a, b)])
 
-gcd_run :: Integral a => a -> a -> (a, [(a, a)])
-gcd_run a b = runWriter (gcd_logSteps a b)
+-- counting step
+gcd_countSteps :: Integral a => a -> a -> Writer (Sum Int) a
+gcd_countSteps = gcdM (\_ _ -> tell $ Sum 1)
+
+-- using mapWriter
+gcd_countSteps' :: Integral b => b -> b -> Writer (Sum Int) b
+gcd_countSteps' a b = mapWriter mapper (gcd_logSteps a b)
+  where
+    mapper (v, w) = (v, Sum $ length w)
+
+-- simplify
+gcd_countSteps'' :: Integral a => a -> a -> Writer (Sum Int) a
+gcd_countSteps'' = (mapWriter (Sum . length <$>) .) . gcd_logSteps
