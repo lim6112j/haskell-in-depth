f0618b603a2f773bff38f73f496f7c7381bf539c
lim6112j <lim6112j@gmail.com>
2022-09-23 02:36:46 +0900

    brilliant code

--- src/TypeClass.hs
+++ src/TypeClass.hs
@@ -1,8 +1,9 @@
---
+{-# LANGUAGE DeriveAnyClass #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
 
 module TypeClass where
 
-data Direction = North | East | South | West deriving (Eq, Enum, Bounded, Show)
+data Direction = North | East | South | West deriving (Eq, Enum, Bounded, Show, CyclicEnum)
 
 data Turn = TNone | TLeft | TRight | TAround deriving (Eq, Enum, Bounded, Show)
 
@@ -16,10 +17,26 @@ class (Eq a, Enum a, Bounded a) => CyclicEnum a where
     | d == maxBound = minBound
     | otherwise = succ d
 
-instance CyclicEnum Direction
+--instance CyclicEnum Direction
 
 rotate :: Turn -> Direction -> Direction
 rotate TNone = id
 rotate TLeft = cpred
 rotate TRight = csucc
 rotate TAround = cpred . cpred
+
+every :: (Enum a, Bounded a) => [a]
+every = enumFrom minBound
+
+orient :: Direction -> Direction -> Turn
+orient d1 d2 = head $ filter (\t -> rotate t d1 == d2) every
+
+rotateMany :: Direction -> [Turn] -> Direction
+rotateMany = foldl (flip rotate)
+
+rotateManyStep :: Direction -> [Turn] -> [Direction]
+rotateManyStep = scanl $ flip rotate
+
+orientMany :: [Direction] -> [Turn]
+orientMany ds@(_ : _ : _) = zipWith orient ds (tail ds)
+orientMany _ = []
