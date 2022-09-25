c04e2ef645d9b0e9a96b6716780497c9303c5ba1
lim6112j <lim6112j@gmail.com>
2022-09-25 20:01:49 +0900

    du component setting

---
+++ du/AppTypes.hs
@@ -0,0 +1,37 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
+module AppTypes where
+
+import Control.Monad.RWS
+import System.PosixCompat
+
+data AppConfig = AppConfig
+  { basePath :: FilePath,
+    maxDepth :: Int,
+    extension :: Maybe String,
+    followSymlinks :: Bool
+  }
+
+data AppEnv = AppEnv
+  { cfg :: AppConfig,
+    path :: FilePath,
+    depth :: Int,
+    fileStatus :: FilePath -> IO FileStatus
+  }
+
+initialEnv :: AppConfig -> AppEnv
+initialEnv config@AppConfig {..} =
+  AppEnv
+    { cfg = config,
+      path = basePath,
+      depth = 0,
+      fileStatus =
+        if followSymlinks
+          then getFileStatus
+          else getSymbolicLinkStatus
+    }
+
+type MyApp logEntry state = RWST AppEnv [logEntry] state IO
+
+runMyApp :: MyApp logEntry state a -> AppConfig -> state -> IO (a, [logEntry])
+runMyApp app config = evalRWST app (initialEnv config)
---
+++ du/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import AppTypes
+
+main :: IO ()
+main = putStrLn "hello"
