59543c25c30ae657776587612aa799f465bf78b0
lim6112j <lim6112j@gmail.com>
2023-01-24 17:40:48 +0900

    recovering

--- ch07/div.hs
+++ ch07/div.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 import Control.Exception
-import Control.Monad.Catch hiding (try)
+import Control.Monad.Catch hiding (catch, tryJust, try)
 import Data.Functor
 import Control.Monad.Writer
 data MyArithException = DivByZero | OtherArithException deriving (Show, Exception)
@@ -15,24 +15,48 @@ divIO x y = pure (x `div` y)
 divM :: MonadThrow m => Int -> Int -> m Int
 divM _ 0 = throwM DivByZero
 divM x y = pure (x `div` y)
-testComputation :: Int -> Int -> Int -> WriterT [Int] IO Int
-testComputation a b c = do 
-  o <- lift $ divIO a b 
+testComputation :: Int -> Int -> Int -> IO Int
+testComputation a b c = do
+  divIO a b >>= divIO c
+
+testComputationWriter :: Int -> Int -> Int -> WriterT [Int] IO Int
+testComputationWriter a b c = do
+  o <- lift $ divIO a b
   tell [a, b, c, o]
   lift $ divIO c o
-
 divTestWithRecovery :: Int -> Int -> Int -> IO Int
 divTestWithRecovery a b c = do
-  (v, w) <- runWriterT $ testComputation a b c
-  print (v, w)
-  try (pure (v, w) ) <&> dealWith 
+  try (testComputation a b c ) <&> dealWith
     where
-      dealWith :: Either MyArithException (Int, [Int]) -> Int
-      dealWith (Right r) = fst r
+      dealWith :: Either MyArithException Int -> Int
+      dealWith (Right r) =  r
       dealWith (Left _) = 0
+divTestWithRecovery2 :: Int -> Int -> Int -> IO Int
+divTestWithRecovery2 a b c =
+  tryJust isDivideByZero (runWriterT $ testComputationWriter a b c) >>= pure . dealWith
+  where
+    dealWith (Right r) = fst r
+    dealWith (Left _) = 0
+    isDivideByZero :: MyArithException -> Maybe ()
+    isDivideByZero DivByZero = Just ()
+    isDivideByZero _ = Nothing
+-- catch & handle
+divTestIO :: Int -> Int -> Int -> IO Int 
+divTestIO a b c = testComputation a b c `catch` handler
+  where
+    handler :: MyArithException -> IO Int
+    handler e = do
+      putStrLn $ "We've got an exception: " ++ show e
+        ++ "\nUsing default value 0"
+      pure 0
 main :: IO ()
 main = do
-  3 `divM` 1 >>= print
-  3 `divIO` 2 >>= print
-  divTestWithRecovery 10 10 0 >>= print
+  3 `divM` 1 >>= print -- no recovering below code will not run when exception
+  3 `divIO` 2 >>= print -- no recovering below code will not run when exception
+  print "divtest1"
+  divTestWithRecovery 10 0 10 >>= print --  try recovered so below code run
+  print "divtest2"
+  divTestWithRecovery2 10 0 10 >>= print  -- tryJust recovered so below code run
+  print "div catch exception"
+  divTestIO 10 0 2 >>= print -- catch and handler , recovered so below code run
   print $ 3 `divPure` 0
