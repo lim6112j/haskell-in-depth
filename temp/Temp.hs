f89fb083c29e709ff4a4cbf6b503d5931063742d
lim6112j <lim6112j@gmail.com>
2023-04-16 01:27:27 +0900

    ghc setting

--- temp/Temp.hs
+++ temp/Temp.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text
+
+type SQL = Text
+
+data ErrorMsg = WrongFormat Int Text deriving (Show)
+
+testData :: Text
+testData = "Pen:Bob\nglass:Mary:10\nPencil:Alice\nBook:Bob\nBottle"
+
+-- getSQL :: Text -> Writer [ErrorMsg] SQL
+-- genSQL txt = undefined
+
 main :: IO ()
 main = do
   putStrLn "hello"
